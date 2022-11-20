02302b1e1b208d6b707c2f480894fc25ae5453c5
Fernando Galvan <n.brighter00@gmail.com>
2022-11-20 12:29:21 -0600

    adding todos

--- JS/practice.js
+++ JS/practice.js
@@ -1,1 +1,9 @@
-let addToDoButton = document.getElementById('addToDo')
+let addToDoButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer')
+let inputField = document.getElementById("inputField")
+
+addToDoButton.addEventListener('click', function(){
+    let paragraph = document.createElement('p')
+    paragraph.innerText = document.getElementById('inputField').value;
+    toDoContainer.appendChild(paragraph)
+})
