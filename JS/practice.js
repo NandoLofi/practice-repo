794718775d9d6a0b3f53c251ce567f91a75c0e28
Fernando Galvan <n.brighter00@gmail.com>
2022-11-20 17:40:38 -0600

    finished todo

--- JS/practice.js
+++ JS/practice.js
@@ -4,6 +4,11 @@ let inputField = document.getElementById("inputField")
 
 addToDoButton.addEventListener('click', function(){
     let paragraph = document.createElement('p')
+    paragraph.classList.add('paragaraph-styling')
     paragraph.innerText = document.getElementById('inputField').value;
     toDoContainer.appendChild(paragraph)
+    inputField.value = []
+    paragraph.addEventListener('click', function(){
+        toDoContainer.removeChild(paragraph)
+    })
 })
