6ea8afa1076278edefe49b18570cf3bfcda8272a
Fernando Galvan <n.brighter00@gmail.com>
2022-11-20 18:36:17 -0600

    simple python game start

---
+++ Python/quizgame.py
@@ -0,0 +1,15 @@
+print("Hello! Welcome to my festivities...")
+
+playing = input("Do you want to play my game? ")
+
+if playing != "yes":
+    quit()
+
+print("Okay...let's play ")
+
+answer = input("What is 2 + 2? ")
+
+if answer == "4":
+    print("Well done player...let's continue")
+else:
+    print("Don't get more wrong...you don't want to do that")
