35c0f3fe3648aaa0d5a8a2141d2774433df96fcb
Fernando Galvan <n.brighter00@gmail.com>
2022-11-21 22:09:50 -0600

    basic to-do day 2

--- JS/practice.js
+++ JS/practice.js
@@ -1,14 +1,17 @@
-let addToDoButton = document.getElementById('addToDo');
-let toDoContainer = document.getElementById('toDoContainer')
-let inputField = document.getElementById("inputField")
+// let addToDoButton = document.getElementById('addToDo');
+// let toDoContainer = document.getElementById('toDoContainer')
+// let inputField = document.getElementById("inputField")
 
-addToDoButton.addEventListener('click', function(){
-    let paragraph = document.createElement('p')
-    paragraph.classList.add('paragaraph-styling')
-    paragraph.innerText = document.getElementById('inputField').value;
-    toDoContainer.appendChild(paragraph)
-    inputField.value = []
-    paragraph.addEventListener('click', function(){
-        toDoContainer.removeChild(paragraph)
-    })
-})
+// addToDoButton.addEventListener('click', function(){
+//     let paragraph = document.createElement('p')
+//     paragraph.classList.add('paragaraph-styling')
+//     paragraph.innerText = document.getElementById('inputField').value;
+//     toDoContainer.appendChild(paragraph)
+//     inputField.value = []
+//     paragraph.addEventListener('click', function(){
+//         toDoContainer.removeChild(paragraph)
+//     })
+// })
+
+let addToDoButton = document.getElementById('addToDo')
+let toDoContainer = document.getElementById('toDoContainer')
