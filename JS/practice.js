3a0859255b186fe2c6515acbdc8f9db734e5d1c3
NandoLofi <n.brighter00@gmail.com>
2022-12-13 19:37:14 -0600

    simple number refresher

--- JS/practice.js
+++ JS/practice.js
@@ -13,17 +13,33 @@
 //     })
 // })
 
-let addToDoButton = document.getElementById('addToDo')
-let toDoContainer = document.getElementById('toDoContainer')
-let inputField = document.getElementById('inputField')
+// let addToDoButton = document.getElementById('addToDo')
+// let toDoContainer = document.getElementById('toDoContainer')
+// let inputField = document.getElementById('inputField')
 
-addToDoButton.addEventListener('click', function(){
-    let paragaraph = document.createElement('p')
-    paragaraph.classList.add('paragraph-styling')
-    paragaraph.innerText = document.getElementById('inputField').value
-    toDoContainer.appendChild(paragaraph)
-    inputField.value = []
-    paragaraph.addEventListener('click', function(){
-        toDoContainer.removeChild(paragaraph)
-    })
-})
+// addToDoButton.addEventListener('click', function(){
+//     let paragaraph = document.createElement('p')
+//     paragaraph.classList.add('paragraph-styling')
+//     paragaraph.innerText = document.getElementById('inputField').value
+//     toDoContainer.appendChild(paragaraph)
+//     inputField.value = []
+//     paragaraph.addEventListener('click', function(){
+//         toDoContainer.removeChild(paragaraph)
+//     })
+// })
+
+//check if is an INT
+
+//Question #1
+
+let arrOne = [1 , 2, 3, 4]
+
+const intFunc = (num)=>{
+    return num % 1 === 0;
+}
+
+console.log(intFunc(arrOne[0]));
+
+
+let sum = arrOne.reduce((acc, curr)=> acc + curr, 0)
+console.log(sum)
